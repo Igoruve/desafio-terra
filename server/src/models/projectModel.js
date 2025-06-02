d5ca0a3afa731718179b587840f101992fac3ebd
igoruve <igoruve@gmail.com>
2025-06-02 16:17:40 +0200

    project model added

---
+++ server/src/models/projectModel.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const projectSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  projectId: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "in progress", "completed", "cancelled"],
+    required: true,
+  },
+  clients: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  ],
+  manager: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  issues: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Issue",
+      required: true,
+    },
+  ],
+});
+
+export default mongoose.model("Project", projectSchema);
