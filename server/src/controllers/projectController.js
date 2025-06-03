0b6c884811c3ccfc15324541a7a904b474cd08a7
igoruve <igoruve@gmail.com>
2025-06-03 10:18:50 +0200

    projectController Modified

--- server/src/controllers/projectController.js
+++ server/src/controllers/projectController.js
@@ -1,45 +1,83 @@
 import projectModel from "../models/projectModel.js";
 import userModel from "../models/userModel.js";
 import issueModel from "../models/issueModel.js";
-
 import { customAlphabet } from "nanoid";
 
 const getRandomCode = customAlphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 6);
 
-const getProjects = () => projectModel.find();
+const getProjects = () =>
+  projectModel
+    .find()
+    .populate("clients")
+    .populate("manager")
+    .populate("issues");
 
-const getProjectById = (id) => projectModel.findById(id.trim());
+const getProjectById = (id) =>
+  projectModel
+    .findById(id.trim())
+    .populate("clients")
+    .populate("manager")
+    .populate("issues");
 
 const getProjectsByUserId = async (userId) => {
   const user = await userModel.findById(userId.trim());
   if (!user) throw new Error("UserNotFound");
-  return projectModel.find({ user: user._id });
+
+  return projectModel
+    .find({ $or: [{ manager: user._id }, { clients: user._id }] })
+    .populate("clients")
+    .populate("manager")
+    .populate("issues");
 };
 
 const getProjectByIssueId = async (issueId) => {
   const issue = await issueModel.findById(issueId.trim());
   if (!issue) throw new Error("IssueNotFound");
-  const project = await projectModel.findById(issue.project);
+
+  const project = await projectModel
+    .findById(issue.project)
+    .populate("clients")
+    .populate("manager")
+    .populate("issues");
+
   if (!project) throw new Error("ProjectNotFound");
   return project;
 };
 
 const getProjectsByDate = (date) =>
-  projectModel.find({ createdAt: { $gte: date } });
+  projectModel
+    .find({ createdAt: { $gte: date } })
+    .populate("clients")
+    .populate("manager")
+    .populate("issues");
 
 const createProject = async (data) => {
+  let projectId;
+  let exists;
+
+  do {
+    projectId = getRandomCode();
+    exists = await projectModel.findOne({ projectId });
+  } while (exists);
+
   const project = new projectModel({
     ...data,
-    projectCode: getRandomCode(),
+    projectId,
   });
+
   return project.save();
 };
 
 const updateProject = async (id, updateData) => {
-  const project = await projectModel.findByIdAndUpdate(id, updateData, {
-    new: true,
-    runValidators: true,
-  });
+  const project = await projectModel
+    .findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true,
+    })
+    .populate("clients")
+    .populate("manager")
+    .populate("issues");
+
   if (!project) throw new Error("ProjectNotFound");
   return project;
 };
