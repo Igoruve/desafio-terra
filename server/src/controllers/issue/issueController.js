f29520cbbcb0c4d7569dcb0a79a47f5ddbe9ad8d
Asier <acasiergonzalez@gmail.com>
2025-06-04 17:21:33 +0200

    issueControllers fixed

--- server/src/controllers/issue/issueController.js
+++ server/src/controllers/issue/issueController.js
@@ -1,49 +1,67 @@
 import issueModel from "../../models/issueModel.js";
 import projectModel from "../../models/projectModel.js";
 
-import { issueTypeNotProvided, 
-    deviceNotProvided, browserNotProvided, 
-    clientCommentNotProvided, pageUrlNotProvided } from "../../utils/errors/issueErrors.js";
+import {
+    issueTypeNotProvided,
+    deviceNotProvided, browserNotProvided,
+    clientCommentNotProvided, pageUrlNotProvided
+} from "../../utils/errors/issueErrors.js";
 
 import { customAlphabet } from "nanoid";
 
 const getRandomCode = customAlphabet("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 6);
 async function getAllIssues() {
-    const issues = await issueModel.find().populate("client");
+    const issues = await issueModel.find().populate({
+        path: "client",
+        select: "-password -apiKey"
+    });
     return issues;
 }
-async function getIssueById (issueId){
-    const issue = await issueModel.findOne({issueId:issueId}).populate("client");
+async function getIssueById(issueId) {
+    const issue = await issueModel.findOne({ issueId: issueId }).populate({
+        path: "client",
+        select: "-password -apiKey"
+    });
     return issue;
 }
 
-async function getIssuesByStatus(status){
-    const issues = await issueModel.find({status:status}).populate("client");
+async function getIssuesByStatus(status) {
+    console.log(status);
+    const issues = await issueModel.find({ status }).populate({
+        path: "client",
+        select: "-password -apiKey"
+    });
     return issues;
 }
 
 
-async function getIssuesByDate(date){
-    const issues = await issueModel.find({date:date}).populate("client");
+async function getIssuesByDate(date) {
+    const issues = await issueModel.find({ createdAt: { $gte: date } }).populate({
+        path: "client",
+        select: "-password -apiKey"
+    });
     return issues;
 }
 
-async function getIssuesByDevice(device){
-    const issues = await issueModel.find({device:device}).populate("client");
+async function getIssuesByDevice(device) {
+    const issues = await issueModel.find({ device: device }).populate({
+        path: "client",
+        select: "-password -apiKey"
+    });
     return issues;
 }
-async function createIssue (projectId,data){
+async function createIssue(projectId, data) {
 
-    if(!data.issueType) throw new issueTypeNotProvided();
-    if(!data.device) throw new deviceNotProvided();
-    if(!data.browser) throw new browserNotProvided();
-    if(!data.clientComment) throw new clientCommentNotProvided();
-    if(!data.page) throw new pageUrlNotProvided();
+    if (!data.issueType) throw new issueTypeNotProvided();
+    if (!data.device) throw new deviceNotProvided();
+    if (!data.browser) throw new browserNotProvided();
+    if (!data.clientComment) throw new clientCommentNotProvided();
+    if (!data.page) throw new pageUrlNotProvided();
 
     const code = getRandomCode();
     data.issueId = code;
 
-    const project = await projectModel.findOne({projectId:projectId}).populate("issues");
+    const project = await projectModel.findOne({ projectId: projectId }).populate("issues");
 
     const newIssue = await issueModel.create(data);
     newIssue.save();
@@ -54,17 +72,17 @@ async function createIssue (projectId,data){
     return newIssue;
 }
 
-async function editIssue(issueId,data){
-    const issue = await issueModel.findOneAndUpdate({issueId:issueId},data,{new:true});
+async function editIssue(issueId, data) {
+    const issue = await issueModel.findOneAndUpdate({ issueId: issueId }, data, { new: true });
     return issue;
 }
 
-async function deleteIssue(issueId){
-    const issue = await issueModel.findOneAndDelete({issueId:issueId});
+async function deleteIssue(issueId) {
+    const issue = await issueModel.findOneAndDelete({ issueId: issueId });
     return issue;
 }
 
-export default{
+export default {
     getAllIssues,
     getIssueById,
     getIssuesByStatus,
