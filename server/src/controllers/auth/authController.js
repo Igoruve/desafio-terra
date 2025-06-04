14ceea983966892521d6261b6eb871b320c091f5
AsGon1 <acasiergonzalez@gmail.com>
2025-06-04 15:20:08 +0200

    Revert "Update user Router"

--- server/src/controllers/auth/authController.js
+++ server/src/controllers/auth/authController.js
@@ -55,8 +55,9 @@ const register = async ({ userData }) => {
   if (existingEmail) throw new UserEmailAlreadyExists();
 
 // Hashear la contraseña
- /*  const hashedPassword = await hash(password, 10);*/
-  const userId = getRandomCode(); 
+ /*  const hashedPassword = await hash(password, 10);
+  console.log(password, hashedPassword); */
+  const userId = getRandomCode();
 
 const newUser = new userModel({
     userId: userId,
