1d8d3d8a148e216bcb60cf70199ab31de4bfa539
igoruve <igoruve@gmail.com>
2025-06-04 12:32:34 +0200

    commit

--- server/src/routes/projectRouter.js
+++ server/src/routes/projectRouter.js
@@ -4,20 +4,32 @@ import { isLoggedInAPI } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
+router.get("/", isLoggedInAPI, projectAPIController.getProjects);
+
+router.get(
+  "/user/:id",
+  isLoggedInAPI,
+  projectAPIController.getProjectsByUserId
+);
+
+router.get(
+  "/date/:date",
+  isLoggedInAPI,
+  projectAPIController.getProjectsByDate
+);
+
+router.get(
+  "/status/:status",
+  isLoggedInAPI,
+  projectAPIController.getProjectByStatus
+);
+
+router.put("/:id", isLoggedInAPI, projectAPIController.editProject);
+
 router.post("/", isLoggedInAPI, projectAPIController.createProject);
 
-router.get("/", isLoggedInAPI,projectAPIController.getProjects);
+router.delete("/:id", isLoggedInAPI, projectAPIController.deleteProject);
 
-router.get("/user/:id", isLoggedInAPI,projectAPIController.getProjectsByUserId);
-
-router.get("/date/:date", isLoggedInAPI,projectAPIController.getProjectsByDate);
-
-router.get("/status/:status", isLoggedInAPI,projectAPIController.getProjectByStatus);
-
-router.put("/:id", isLoggedInAPI,projectAPIController.editProject);
-
-router.delete("/:id", isLoggedInAPI,projectAPIController.deleteProject);
-
-router.get("/:id", isLoggedInAPI,projectAPIController.getProjectById);
+router.get("/:id", isLoggedInAPI, projectAPIController.getProjectById);
 
 export default router;
