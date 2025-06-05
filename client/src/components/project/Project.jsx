3017dcbf7247687a2879a2e2eeba727a70a2a2d4
igoruve <igoruve@gmail.com>
2025-06-05 17:09:45 +0200

    project added working

--- client/src/components/project/Project.jsx
+++ client/src/components/project/Project.jsx
@@ -1,17 +1,23 @@
 import { useEffect, useState, useRef } from "react";
-import { useLoaderData, useParams, useRevalidator } from "react-router-dom";
+import {
+  useLoaderData,
+  useParams,
+  useRevalidator,
+  Link,
+} from "react-router-dom";
 
 function Project() {
-    const project = useLoaderData();    
-    const { projectId } = useParams();
-    const revalidator = useRevalidator();
-    
+  const project = useLoaderData();
 
-return(
-    <section className="flex flex-col items-center justify-center h-screen bg-gray-800">
-        <div className="bg-white text-black w-fit h-fit"><p>{project.title}</p></div>
+  return (
+    <section className="flex flex-col items-center justify-start h-screen bg-[var(--bg-color)] pt-16">
+      <div className="bg-white/80 text-black w-full h-fit px-8 py-4 my-4">
+        <Link to="/issues">
+          <p>{project.title}</p>
+        </Link>
+      </div>
     </section>
-)
+  );
 }
 
 export default Project;
