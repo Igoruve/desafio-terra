7be95210d36409bbc1e55ed2ea17a2dd8a9c68b6
igoruve <igoruve@gmail.com>
2025-06-05 17:09:45 +0200

    project added working

--- client/src/components/project/ProjectList.jsx
+++ client/src/components/project/ProjectList.jsx
@@ -4,13 +4,16 @@ import { useState } from "react";
 
 import { getProjectsByUserId } from "../../utils/project.js";
 
+import Project from "./Project.jsx";
+
 const ProjectList = () => {
   const navigate = useNavigate();
   const loaderData = useLoaderData();
+  const [projects, setProjects] = useState(loaderData);
 
   return (
     <section>
-      
+      <h2 className="text-white/80 text-xl py-8">Projects</h2>
     </section>
   );
 };
