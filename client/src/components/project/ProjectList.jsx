718b5852a312765d7b961bf744ba31adf5575119
igoruve <igoruve@gmail.com>
2025-06-05 13:15:52 +0200

    fixed get token

--- client/src/components/project/ProjectList.jsx
+++ client/src/components/project/ProjectList.jsx
@@ -2,14 +2,18 @@ import { useLoaderData, useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
-const IssueList = () => {
+import { createProject, deleteProject } from "../../utils/project.js";
+
+const ProjectList = () => {
     const navigate = useNavigate();
     const loaderData = useLoaderData();
     const [search, setSearch] = useState("");
 
 
-    return ()
+    return (
+        <h1>hola</h1>
+    )
 
 }
 
-export default IssueList;
+export default ProjectList;
