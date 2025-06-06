03055b35aee634ab543b6ece18b0aa18eef4dc04
igoruve <igoruve@gmail.com>
2025-06-06 15:35:17 +0200

    form styles fixed

--- client/src/components/form/Form.jsx
+++ client/src/components/form/Form.jsx
@@ -2,7 +2,9 @@ import { useState } from "react";
 
 import { createIssue } from "../../utils/issue";
 
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
+
+import Arrow from "./Arrow.jsx";
 
 const issueTypes = [
   "Copy revision",
@@ -34,7 +36,10 @@ const topBrowsers = [
 
 function Form() {
   const { projectId } = useLoaderData();
+
   const [expanded, setExpanded] = useState(false);
+
+  const navigate = useNavigate();
 
   const handleSubmitForm = async (e) => {
     e.preventDefault();
@@ -50,6 +55,7 @@ function Form() {
       image: form.image.files[0] || null,
     };
     const result = await createIssue(projectId, data);
+    navigate(`/`); //TODO CAMBIAR RUTA
   };
 
   const handleIssueTypeChange = (e) => {
@@ -57,33 +63,38 @@ function Form() {
   };
 
   return (
-    <section className="flex flex-col items-center justify-center h-ful bg-[var(--bg-color)] text-white">
+    <section className="flex flex-col items-center justify-center h-ful bg-[var(--bg-color)] text-white pt-24">
+      <h2 className="text-4xl font-bold ">Create an Issue</h2>
       <form
         onSubmit={handleSubmitForm}
         action=""
-        className="flex flex-col gap-4 items-center justify-center w-full max-w-md mx-auto h-full my-24 text-xl"
+        className="flex flex-col gap-4 items-center justify-center w-full max-w-md mx-auto min-h-screen my-8 px-4 text-base sm:text-xl
+"
       >
-        <fieldset className="w-full p-4 border-3 border-[#ffb410] rounded-xl flex flex-col justify-start items-start">
+        <fieldset className="w-full px-8 py-4 border-3 border-[#ffb410] rounded-xl flex flex-col">
           <legend className="text-lg font-semibold mb-2">Issue Details</legend>
 
           <label htmlFor="issueType" className="pb-4">
             Issue Type*
           </label>
-          <select
-            id="issueType"
-            name="issueType"
-            required
-            onChange={handleIssueTypeChange}
-            className="appearance-none bg-[var(--bg-color)] border-3 border-white rounded-[50px] px-4 py-2 mb-4 cursor-pointer"
-          >
-            <option value="">Select an option</option>
-            {issueTypes.map((type) => (
-              <option key={type} value={type}>
-                {type}
-              </option>
-            ))}
-            <option value="Other">Other</option>
-          </select>
+          <div className="flex flex-row relative w-full items-center justify-between">
+            <select
+              id="issueType"
+              name="issueType"
+              required
+              onChange={handleIssueTypeChange}
+              className="appearance-none pr-10 bg-[var(--bg-color)] border-3 border-white rounded-[50px] px-4 py-2 mb-4 cursor-pointer w-full"
+            >
+              <option value="">Select an option</option>
+              {issueTypes.map((type) => (
+                <option key={type} value={type}>
+                  {type}
+                </option>
+              ))}
+              <option value="Other">Other</option>
+            </select>
+            <Arrow />
+          </div>
 
           {expanded && (
             <>
@@ -93,7 +104,7 @@ function Form() {
               <textarea
                 id="specifyIssue"
                 name="specifyIssue"
-                className="appearance-none bg-[var(--bg-color)] border-3 border-white rounded-[20px] px-4 py-2 mb-4 h-fit max-h-60 w-full cursor-pointer "
+                className="appearance-none bg-[var(--bg-color)] border-3 border-white rounded-[20px] px-4 py-2 mb-4 h-fit max-h-60 w-full cursor-text"
               ></textarea>
             </>
           )}
@@ -101,20 +112,23 @@ function Form() {
           <label htmlFor="status" className="pb-4">
             Status*
           </label>
-          <select
-            id="status"
-            name="status"
-            defaultValue="On Hold"
-            required
-            className="appearance-none bg-[var(--bg-color)] border-3 border-white rounded-[50px] px-4 py-2 mb-4 cursor-pointer "
-          >
-            <option value="On Hold">On Hold</option>
-            {statusOptions.map((status) => (
-              <option key={status} value={status}>
-                {status}
-              </option>
-            ))}
-          </select>
+          <div className="flex flex-row relative w-full items-center justify-between">
+            <select
+              id="status"
+              name="status"
+              defaultValue="On Hold"
+              required
+              className="appearance-none pr-10 bg-[var(--bg-color)] border-3 border-white rounded-[50px] px-4 py-2 mb-4 cursor-pointer w-full"
+            >
+              <option value="On Hold">On Hold</option>
+              {statusOptions.map((status) => (
+                <option key={status} value={status}>
+                  {status}
+                </option>
+              ))}
+            </select>
+            <Arrow />
+          </div>
         </fieldset>
 
         <fieldset className="w-full p-4 border-3 border-[#7ce55e] rounded-xl flex flex-col justify-start items-start">
@@ -125,37 +139,43 @@ function Form() {
           <label htmlFor="device" className="pb-4">
             Device
           </label>
-          <select
-            id="device"
-            name="device"
-            defaultValue="Desktop"
-            required
-            className="appearance-none bg-[var(--bg-color)] border-3 border-white rounded-[50px] px-4 py-2 mb-4 cursor-pointer "
-          >
-            {deviceOptions.map((device) => (
-              <option key={device} value={device}>
-                {device}
-              </option>
-            ))}
-          </select>
+          <div className="flex flex-row relative w-full items-center justify-between">
+            <select
+              id="device"
+              name="device"
+              defaultValue="Desktop"
+              required
+              className="appearance-none pr-10 bg-[var(--bg-color)] border-3 border-white rounded-[50px] px-4 py-2 mb-4 cursor-pointer w-full"
+            >
+              {deviceOptions.map((device) => (
+                <option key={device} value={device}>
+                  {device}
+                </option>
+              ))}
+            </select>
+            <Arrow />
+          </div>
 
           <label htmlFor="browser" className="pb-4">
             Browser
           </label>
-          <select
-            id="browser"
-            name="browser"
-            defaultValue="Google Chrome"
-            required
-            className="appearance-none bg-[var(--bg-color)] border-3 border-white rounded-[50px] px-4 py-2 mb-4 cursor-pointer "
-          >
-            {topBrowsers.map((browser) => (
-              <option key={browser} value={browser}>
-                {browser}
-              </option>
-            ))}
-            <option value="Other">Other</option>
-          </select>
+          <div className="flex flex-row relative w-full items-center justify-between">
+            <select
+              id="browser"
+              name="browser"
+              defaultValue="Google Chrome"
+              required
+              className="appearance-none pr-10 bg-[var(--bg-color)] border-3 border-white rounded-[50px] px-4 py-2 mb-4 cursor-pointer w-full"
+            >
+              {topBrowsers.map((browser) => (
+                <option key={browser} value={browser}>
+                  {browser}
+                </option>
+              ))}
+              <option value="Other">Other</option>
+            </select>
+            <Arrow />
+          </div>
         </fieldset>
 
         <fieldset className="w-full p-4 border-3 border-[#3D9DD8] rounded-xl flex flex-col justify-start items-start">
@@ -171,19 +191,20 @@ function Form() {
             name="comment"
             rows="4"
             required
-            className="appearance-none bg-[var(--bg-color)] border-3 border-white rounded-[20px] px-4 py-2 mb-4 h-fit max-h-60 w-full cursor-pointer "
+            className="appearance-none bg-[var(--bg-color)] border-3 border-white rounded-[20px] px-4 py-2 mb-4 h-fit max-h-60 w-full cursor-text "
           ></textarea>
 
           <label htmlFor="url" className="pb-4">
             Page URL*
           </label>
-          <textarea
+          <input
+            type="text"
             id="url"
             name="url"
             rows="2"
             required
-            className="appearance-none bg-[var(--bg-color)] border-3 border-white rounded-[20px] px-4 py-2 mb-4 max-h-60 w-full resize-none overflow-hidden whitespace-nowrap h-12"
-          ></textarea>
+            className="appearance-none bg-[var(--bg-color)] border-3 border-white rounded-[20px] px-4 py-2 mb-4 max-h-60 w-full resize-none overflow-hidden cursor-text  h-12"
+          ></input>
 
           <label htmlFor="image" className="pb-4">
             Screenshot
@@ -193,7 +214,7 @@ function Form() {
             id="image"
             name="image"
             accept="image/*"
-            className="appearance-none bg-[var(--bg-color)] border-3 border-white rounded-[50px] px-4 py-2 mb-4 cursor-pointer "
+            className="appearance-none bg-[var(--bg-color)] border-3 border-white rounded-[50px] px-4 py-2 mb-4 cursor-pointer w-full"
           />
         </fieldset>
 
