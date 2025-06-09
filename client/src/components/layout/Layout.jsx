f217af0ec8caed74c28fd713a743e7ce7bc8e769
igoruve <igoruve@gmail.com>
2025-06-09 20:32:47 +0200

    style changes to projects

--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import TopNavbar from "../navbar/TopNavbar";
 import Footer from "../footer/Footer";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "../scrollTop/ScrollTop"; 
 
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <TopNavbar />
       <main className="flex-1 overflow-auto">
+        <ScrollToTop />
         <Outlet />
       </main>
       <footer className="bg-[var(--bg-color)] text-white">
@@ -15,6 +17,5 @@ const Layout = () => {
     </div>
   );
 };
-
 
 export default Layout;
